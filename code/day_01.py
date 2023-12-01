6bdd8c3b3a856e52642cd45e21aced19c9b1dda8
bela <bela.britzke@gmx.net>
2023-12-01 07:36:57 +0100

    day01 created function to find first+last occurence of (string)number

--- code/day_01.py
+++ code/day_01.py
@@ -1,26 +1,18 @@
 with open("inputs/day_01", "r") as f:
     data = f.read()
 
-ls = []
-for l in data.split("\n"):
-    d = [int(a) for a in l if a.isdigit()]
-    if len(d)>0:
-        ls.append(10*d[0]+d[-1])
-out = sum(ls)
+fdg = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
+def find_first_digit(s, rng, only_digit=True):
+    for i in rng:
+        if s[i].isdigit():
+            return int(s[i])
+        elif not only_digit:
+            for k,v in fdg.items():
+                if s[i:].startswith(k):
+                    return v
 
-dg = {"one":1, "two":2, "thr":3, "fou":4, "fiv":5, "six":6, "sev":7, "eig":8, "nin":9}
-fdg = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+# part 1
+print(f"1) {sum([find_first_digit(l, range(len(l)))*10 + find_first_digit(l, range(len(l)-1, -1, -1)) for l in data.split("\n")])}")
 
-ls2 = []
-for l in data.split("\n"):
-    d = []
-    for pos,v in enumerate(l):
-        if v.isdigit():
-            d.append(int(v))
-        elif any((l[pos:pos+i] in fdg) for i in [3,4,5]):
-            d.append(dg[l[pos:pos+3]])
-    ls2.append(d[0]*10+d[-1])
-out2 = sum(ls2)
-
-print(f"1) {out}")
-print(f"2) {out2}")
+# part 2
+print(f"2) {sum([find_first_digit(l, range(len(l)), False)*10 + find_first_digit(l, range(len(l)-1, -1, -1), False) for l in data.split("\n")])}")
