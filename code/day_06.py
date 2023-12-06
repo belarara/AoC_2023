c8ed69ee4989fe4157b7d269542a566a8e06d449
bela <bela.britzke@gmx.net>
2023-12-06 14:09:42 +0100

    removed functools from day 06

--- code/day_06.py
+++ code/day_06.py
@@ -1,4 +1,4 @@
-import time, re, functools, numpy
+import time, re, numpy
 start_time = time.time()
 
 with open("inputs/day_06.txt", "r") as f:
@@ -8,5 +8,5 @@ limits, records = [re.findall("(\d+)", data[i]) for i in range(2)]
 better_races = lambda lim, record: int(abs(numpy.diff(numpy.ceil(numpy.roots([-1, lim, -record]))))[0])
 print(f"1) {numpy.prod([better_races(int(lim), int(rec)) for lim, rec in zip(limits, records)])}")
 
-print(f"2) {better_races(*[int(functools.reduce(lambda x,y: x+y, a)) for a in (limits, records)])}")
+print(f"2) {better_races(int(''.join(limits)), int(''.join(records)))}")
 print(f"time: {time.time() - start_time}s")
