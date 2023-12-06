47c424124bb3cf4b954d4a832a2f964adca98398
bela <bela.britzke@gmx.net>
2023-12-06 13:57:34 +0100

    code 06 improve

--- code/day_06.py
+++ code/day_06.py
@@ -4,9 +4,9 @@ start_time = time.time()
 with open("inputs/day_06.txt", "r") as f:
     data = f.read().split("\n")
 
-limits, records = [int(a) for a in re.findall("(\d+)", data[0])], [int(a) for a in re.findall("(\d+)", data[1])]
-print(f"1) {functools.reduce(lambda x,y: x*y, [len([i*(lim-i) for i in range(lim) if i*(lim-i)>records[curr]]) for curr, lim in enumerate(limits)])}")
+limits, records = [re.findall("(\d+)", data[i]) for i in range(2)]
+better_races = lambda lim, record: int(abs(numpy.diff(numpy.ceil(numpy.roots([-1, lim, -record]))))[0])
+print(f"1) {numpy.prod([better_races(int(lim), int(rec)) for lim, rec in zip(limits, records)])}")
 
-lim, record = [int(a) for a in re.findall("(\d+)", data[0].replace(" ", "")+data[1].replace(" ", ""))]
-print(f"2) {int(abs(numpy.diff(numpy.ceil(numpy.roots([-1, lim, -record]))))[0])}")
+print(f"2) {better_races(*[int(functools.reduce(lambda x,y: x+y, a)) for a in (limits, records)])}")
 print(f"time: {time.time() - start_time}s")
