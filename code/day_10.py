d00ebda1cb87b412a4452a4bd765a2a1c9e6b5ec
bela <bela.britzke@gmx.net>
2023-12-10 18:50:29 +0100

    day 10 removed no prints

--- code/day_10.py
+++ code/day_10.py
@@ -7,30 +7,21 @@ with open("inputs/day_10.txt", "r") as f:
 dir_to_coord = {0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0)}
 transform = {"|": (1,3), "-": (0,2), "L": (0,3), "J": (2,3), "7": (1,2), "F": (0,1)}
 xmin,xmax,ymin,ymax = 0, len(data)-1, 0, len(data[0])-1
-print("MIN/MAX", xmin,xmax,ymin,ymax)
 row,col = 0,0
 for i,p in enumerate(data):
     for j,q in enumerate(p):
         if q=="S":
             row,col = i,j
-for i in range(4):
-    x,y = row+dir_to_coord[i][0], col+dir_to_coord[i][1]
-    if (i+2)%4 in transform[data[x][y]]:
+adj = lambda x,y,i: (x+dir_to_coord[i%4][0], y+dir_to_coord[i%4][1])
+for istart in range(4):
+    xstart,ystart = adj(row, col, istart)
+    if (istart+2)%4 in transform[data[xstart][ystart]]:
         break
 s1 = 1
-right, left, pipe = set(), set(), set()
-dirs = []
+x,y,i = xstart, ystart, istart
 while x!=row or y!=col:
-    right.add((x+dir_to_coord[(i-1)%4][0], y+dir_to_coord[(i-1)%4][1]))
-    left.add((x+dir_to_coord[(i+1)%4][0], y+dir_to_coord[(i+1)%4][1]))
-    pipe.add((x,y))
-    i = (i+2)%4
-    dirs.append((x,y,i))
-    symbol = data[x][y]
-    i = [a for a in transform[symbol] if a!=i][0]
-    right.add((x+dir_to_coord[(i-1)%4][0], y+dir_to_coord[(i-1)%4][1]))
-    left.add((x+dir_to_coord[(i+1)%4][0], y+dir_to_coord[(i+1)%4][1]))
-    x,y = x+dir_to_coord[i][0], y+dir_to_coord[i][1]
+    i = [a for a in transform[data[x][y]] if a!=(i+2)%4][0]
+    x,y = adj(x, y, i)
     s1+=1
 print(f"1) {s1//2}")
 
@@ -50,37 +41,15 @@ def expand(side, pipe):
                 if (p,q) not in fields and (p,q) not in pipe and (p,q) not in new: new.append((p,q))
     return fields, border
 
+right, left, pipe = set(), set(), set()
+x,y,i = xstart, ystart, istart
+while x!=row or y!=col:
+    right.add(adj(x,y,i+1)), left.add(adj(x,y,i-1)), pipe.add((x,y))
+    i = [a for a in transform[data[x][y]] if a!=(i+2)%4][0]
+    right.add(adj(x,y,i+1)), left.add(adj(x,y,i-1))
+    x,y = x+dir_to_coord[i][0], y+dir_to_coord[i][1]
+
 pipe.add((row,col))
 fields_left, f1 = expand(left, pipe)
-fields_right, f2 = expand(right, pipe)
-print(f"2) {len(fields_left if not f1 else fields_right)}")
+print(f"2) {len(fields_left if not f1 else expand(right, pipe)[0])}")
 print(f"time: {time.time() - start_time}s")
-
-for x in range(xmax):
-    for y in range(ymax):
-        line = list(data[x])
-        line[y] = "@"
-        data[x] = "".join(line)
-
-for x,y in fields_left:
-    line = list(data[x])
-    line[y] = "O"
-    data[x] = "".join(line)
-
-for x,y in fields_right:
-    line = list(data[x])
-    line[y] = "@"
-    data[x] = "".join(line)
-
-for x,y in (pipe):
-    line = list(data[x])
-    line[y] = "#"
-    data[x] = "".join(line)
-
-for x,y,i in dirs:
-    line = list(data[x])
-    line[y] = ">v<^"[i]
-    data[x] = "".join(line)
-
-for l in data:
-   print(l)
